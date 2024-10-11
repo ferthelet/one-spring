80574e6c23c37d61857207486f66c3047c6204f0
Juan Salazar <ferthelet@gmail.com>
2024-10-11 12:18:48 -0500

    agrega proyecto maven con initlzr, vscode no archetype y archetype quickstart to caompare

---
+++ screenmatch02/src/main/java/com/aluracursos/App.java
@@ -0,0 +1,13 @@
+package com.aluracursos;
+
+/*
+ * Using VSCode Maven Project 
+ * with Archetype Quickstart
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

6673afd3c1b37c1cc042bb78c00dacbadbb662f0
Juan Salazar <ferthelet@gmail.com>
2024-10-12 17:23:53 -0500

    agrega @JsonAlias() para mapear nombres de atributos

--- screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -1,6 +1,13 @@
 package com.aluracursos.screenmatch.model;
 
-public record DatosSerie(String titulo, Integer totalDeTemporadas, String evaluacion) {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DatosSerie(
+    // @JsonProperty() sirve para leer el nombre de la propiedad en el JSON y asignarlo al atributo de la clase
+    // @JsonAlias() sirve para leer solamente
+    @JsonAlias("Title") String titulo,
+    @JsonAlias ("totalSeasons") Integer totalDeTemporadas,
+    @JsonAlias("imdbRating") String evaluacion) {
 
 
 }

7c092faf24fe90312b7c0087bad62f05238a530b
Juan Salazar <ferthelet@gmail.com>
2024-10-13 14:53:55 -0500

    implementa interfaz en clase y agrega try/catch

--- screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -1,13 +1,16 @@
 package com.aluracursos.screenmatch.service;
 
 import com.aluracursos.screenmatch.model.DatosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConvierteDatos {
+public class ConvierteDatos implements IConvierteDatos {
+
+    private ObjectMapper om = new ObjectMapper();
     //
     // favorece a una interfaz en case que se necesite Serie o Pelicula
     //
-    // private ObjectMapper om = new ObjectMapper();
     // public DatosSerie obtenerDatos(String json) {
     //     try {
     //         return om.readValue(json, DatosSerie.class);
@@ -15,4 +18,15 @@ public class ConvierteDatos {
     //         throw new RuntimeException("Error al convertir JSON a DatosSerie", e);
     //     }
     // }
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return om.readValue(json, clase);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }

04b2af4b7b82fb49b06ee5b726f29a3de8e8146c
Juan Salazar <ferthelet@gmail.com>
2024-10-13 14:50:03 -0500

    hace clase ConvierteDatos con su interfaz generica

--- screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -4,12 +4,15 @@ import com.aluracursos.screenmatch.model.DatosSerie;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConvierteDatos {
-    private ObjectMapper om = new ObjectMapper();
-    public DatosSerie obtenerDatos(String json) {
-        try {
-            return om.readValue(json, DatosSerie.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Error al convertir JSON a DatosSerie", e);
-        }
-    }
+    //
+    // favorece a una interfaz en case que se necesite Serie o Pelicula
+    //
+    // private ObjectMapper om = new ObjectMapper();
+    // public DatosSerie obtenerDatos(String json) {
+    //     try {
+    //         return om.readValue(json, DatosSerie.class);
+    //     } catch (Exception e) {
+    //         throw new RuntimeException("Error al convertir JSON a DatosSerie", e);
+    //     }
+    // }
 }

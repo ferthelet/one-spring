0bc609aeec129ae32aa67f272df8250e9387e0d2
Juan Salazar <ferthelet@gmail.com>
2024-10-16 19:36:54 -0500

    corrige nombre record y agrega var episodios

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
@@ -26,9 +27,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var ConsumoAPI = new ConsumoAPI();
 		var json = ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
 		// var json = ConsumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+		// System.out.println(json);
 		ConvierteDatos conversor = new ConvierteDatos();
 		var datos = conversor.obtenerDatos(json, DatosSerie.class);
 		System.out.println(datos);
+
+		// episodio
+		json = ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=4fc7c187");
+		var episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodios);
 	}
 }
--- screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosEpisodio.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosEpisodio.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosEpisodios(
+public record DatosEpisodio (
     @JsonAlias("Title") String titulo,
     @JsonAlias("Episode") Integer numeroEpisodio,
     @JsonAlias("imdbRating") String evaluacion,

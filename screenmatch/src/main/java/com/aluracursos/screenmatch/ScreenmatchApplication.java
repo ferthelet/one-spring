87adc3fd856ca21878b27c8a53938adf4beb6516
Juan Salazar <ferthelet@gmail.com>
2024-10-16 21:27:41 -0500

    comenta el contenido previo para llamar a principal solamente

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.principal.Principal;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 
@@ -27,28 +28,33 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// System.out.println("Hola Mundo desde Spring");
-		var ConsumoAPI = new ConsumoAPI();
-		var json = ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+		// // System.out.println("Hola Mundo desde Spring");
+		// var ConsumoAPI = new ConsumoAPI();
 		// var json =
-		// ConsumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-		// System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
+		// ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+		// // var json =
+		// // ConsumoAPI.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		// // System.out.println(json);
+		// ConvierteDatos conversor = new ConvierteDatos();
+		// var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		// System.out.println(datos);
 
-		// episodio
-		json = ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=4fc7c187");
-		var episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodios);
+		// // episodio
+		// json =
+		// ConsumoAPI.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=4fc7c187");
+		// var episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+		// System.out.println(episodios);
 
 		// temporadas
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
-			json = ConsumoAPI
-					.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=4fc7c187");
-			temporadas.add(conversor.obtenerDatos(json, DatosTemporadas.class));
-		}
-		temporadas.forEach(System.out::println);
+		// List<DatosTemporadas> temporadas = new ArrayList<>();
+		// for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+		// json = ConsumoAPI
+		// .obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i +
+		// "&apikey=4fc7c187");
+		// temporadas.add(conversor.obtenerDatos(json, DatosTemporadas.class));
+		// }
+		// temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.muestraElMenu();
 	}
 }

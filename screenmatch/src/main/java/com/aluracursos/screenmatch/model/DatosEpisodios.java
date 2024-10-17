3b1b1e6fde9183d8e5475ec4e0bb88c6b2d3808f
Juan Salazar <ferthelet@gmail.com>
2024-10-16 19:23:53 -0500

    agrega anotacion igonore properties

--- screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosEpisodios.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/model/DatosEpisodios.java
@@ -1,7 +1,9 @@
 package com.aluracursos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosEpisodios(
     @JsonAlias("Title") String titulo,
     @JsonAlias("Episode") Integer numeroEpisodio,

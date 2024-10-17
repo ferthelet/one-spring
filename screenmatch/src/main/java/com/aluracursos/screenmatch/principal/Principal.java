b302651000f87185a2b4dd2d48380a3c76bd1b69
Juan Salazar <ferthelet@gmail.com>
2024-10-16 21:25:04 -0500

    busca datos temporada

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -1,23 +1,35 @@
 package com.aluracursos.screenmatch.principal;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 
 public class Principal {
     private Scanner lector = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private final String API_KEY = "&apikey=4fc7c187";
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-        
+    private ConvierteDatos conversor = new ConvierteDatos();
+
     public void muestraElMenu() {
         System.out.println("Bienvenido al Screenmatch");
         System.out.println("Escribe el nombre de la serie que deseas buscar:");
         var nombreSerie = lector.nextLine();
-
         var json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
 
-        System.out.println(json);
+        // busca datos de las temporadaas
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+            json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+            temporadas.add(conversor.obtenerDatos(json, DatosTemporadas.class));
+        }
+        temporadas.forEach(System.out::println);
 
     }
 

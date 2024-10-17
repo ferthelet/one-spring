c3161089e5af9f5e0427bae1e84b417677f0efcf
Juan Salazar <ferthelet@gmail.com>
2024-10-16 21:05:05 -0500

    agrega instancia consumoAPI

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -2,8 +2,11 @@ package com.aluracursos.screenmatch.principal;
 
 import java.util.Scanner;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+
 public class Principal {
     private Scanner lector = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
     
     public void muestraElMenu() {
         System.out.println("Bienvenido al Screenmatch");

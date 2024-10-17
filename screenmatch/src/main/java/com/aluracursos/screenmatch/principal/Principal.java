c253b91d7520f92f1920e5d9ceb6594ca24fe619
Juan Salazar <ferthelet@gmail.com>
2024-10-16 21:16:17 -0500

    agrega json y sout para probar mas adelante

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -7,11 +7,17 @@ import com.aluracursos.screenmatch.service.ConsumoAPI;
 public class Principal {
     private Scanner lector = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    
+    private final String API_KEY = "&apikey=4fc7c187";
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+        
     public void muestraElMenu() {
         System.out.println("Bienvenido al Screenmatch");
         System.out.println("Escribe el nombre de la serie que deseas buscar:");
         var nombreSerie = lector.nextLine();
+
+        var json = consumoAPI.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+
+        System.out.println(json);
 
     }
 
